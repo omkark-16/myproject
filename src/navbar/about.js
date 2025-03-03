37ff1782abba31d2f268b30ba70498dba78b2ac6
omkark-16 <155129274+omkark-16@users.noreply.github.com>
2025-03-03 15:00:37 +0530

    commit

--- src/navbar/about.js
+++ src/navbar/about.js
@@ -1,7 +1,7 @@
 export default function About(){
     return (
         <div>
-            <center><h1 style={{paddingTop:"250px"}} >About Us</h1></center> 
+            <center><h1 style={{paddingTop:"250px"}} >Welcome here| About Us</h1></center> 
             
         </div>
     )
